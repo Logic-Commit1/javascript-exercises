abb4f1a19a4a70c8c911bacdd41b62150bbbcea8
Logic <logictanjueco@gmail.com>
2022-09-25 09:10:11 +0800

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (x) {
+  let result = 0;
+  for (const value of x) {
+    result += value;
+  }
+  return result;
 };
 
-const multiply = function() {
+const multiply = function (x) {
+  let result = 1;
+  for (const value of x) {
+    result *= value;
+  }
+  return result;
+};
+
+const power = function (x, y) {
+  let result = x * x;
+  for (i = 1; i < y - 1; i++) {
+    result *= x;
+  }
+  return result;
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x <= 1) {
+    return 1;
+  } else {
+    let result = x * (x - 1);
+    for (i = x; i > 3; i--) {
+      result = result * (i - 2);
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
