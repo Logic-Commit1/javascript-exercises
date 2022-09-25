47e98f9db326ebad5153da457d46a0b6fe64093b
Logic <logictanjueco@gmail.com>
2022-09-25 19:32:28 +0800

    finished palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let rawText = text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+    
+    
+    let reversed = ''
+    for (let i = 1; i <= rawText.length; i++) {
+        reversed += rawText[rawText.length-i]
+    }
+    return(rawText==reversed);
 };
 
 // Do not edit below this line
